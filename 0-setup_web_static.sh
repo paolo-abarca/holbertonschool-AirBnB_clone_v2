c2fff58e6b75479634af145caf46cc2ce2f95c82
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-08-25 14:12:52 -0500

    Adding Bash script that configures web servers for web_static implementation

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,13 +3,13 @@
 sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y install nginx
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-new_string="server_name _;\n\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}"
+new_string="server_name _;\n\n\tlocation \/hbnb_static\/ \{\n\t\talias \/data\/web_static\/current\/;\n\t\}"
 
 sudo sed -i "s/server_name _;/$new_string/" /etc/nginx/sites-available/default
 
-sudo service nginx start
+sudo service nginx restart
