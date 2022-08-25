724bb81db86b81bc951002915c024a354a169ef8
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-08-25 14:02:43 -0500

    Fixing Bash script that configures web servers for web_static implementation

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,8 +5,7 @@ sudo apt -y upgrade
 sudo apt -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo touch /data/web_static/releases/test/index.html
-echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > /data/web_static/releases/test/index.html
+echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 new_string="server_name _;\n\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}"
