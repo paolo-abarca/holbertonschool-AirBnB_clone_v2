4bdd78cd771ec972f945383e3e06b538ddbb7c0f
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-08-25 12:36:42 -0500

    Fixing Bash script that configures web servers for web_static implementation

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,11 +4,11 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 sudo touch /data/web_static/releases/test/index.html
-echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-new_string="server_name _;\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t\tindex index.html index.htm;\n\t}"
+sudo echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+new_string="server_name _;\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t\tautoindex off;\n\t}"
 
-sed -i "s/server_name _;/$new_string/" /etc/nginx/sites-available/default
+sudo sed -i "s/server_name _;/$new_string/" /etc/nginx/sites-available/default
 
 sudo service nginx restart
