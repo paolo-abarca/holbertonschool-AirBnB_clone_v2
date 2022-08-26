47436ec72bf25d5064b76588a52de9ee1965111f
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-08-25 19:44:15 -0500

    new version para un test

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-# Adding Bash script that configures web servers for web_static implementation
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+# Bash script that sets up your web servers for the deployment of web_static
+
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y install nginx
+sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+echo "Hello World" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-new_string="server_name _;\n\n\tlocation \/hbnb_static\/ \{\n\t\talias \/data\/web_static\/current\/;\n\t\}"
+chown -hR ubuntu:ubuntu /data/
+search="^\t\}$"
+adding="\t\}\n\n\tlocation \/hbnb_static\/ \{\n\t\talias \/data\/web_static\/current\/;\n\t\}"
 
-sudo sed -i "s/server_name _;/$new_string/" /etc/nginx/sites-available/default
-
+sudo sed -i "0,/$search/s//$adding/" /etc/nginx/sites-available/default
 sudo service nginx restart
