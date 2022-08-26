2cd648b1ecd488be5b40201bfd6b5ad8445566b6
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-08-25 23:59:37 -0500

    updating file

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" "Full deployment """
+"""Adding a Fabric script (based on the 2-do_deploy_web_static.py
+file) that creates and deploys a file to your web servers,
+using the deploy function"""
 
 
 from fabric.operations import local, run, put
@@ -7,14 +9,12 @@ from datetime import datetime
 from os.path import getsize
 from fabric.api import env
 import os
-env.hosts = ['34.226.205.91', '54.163.37.58']
+env.hosts = ['54.147.136.182', '54.163.40.93']
 
 
 def do_pack():
-    """  script that generates a .tgz archive from the contents of the
-    web_static folder of your AirBnB Clone repo, using the function
-    do_pack"""
-
+    """generates a .tgz file from the contents
+    of the web_static folder of your AirBnB Clone repository"""
     local("mkdir -p versions")
     date = datetime.utcnow()
     format_date = "%Y%m%d%H%M%S"
@@ -30,28 +30,20 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """ script (based on the file 1-pack_web_static.py) that distributes
-    an archive to your web servers, using the function do_deploy:"""
+    """generates a .tgz file from the contents
+    of the web_static folder of your AirBnB Clone repository"""
     if os.path.exists(archive_path):
         archive = archive_path.split('/')[1]
-        # [ versions, web_static_20170315003959.tgz]
         filename = archive.split('.')[0]
-        # [web_static_20170315003959, tgz]
         path = '/data/web_static/releases/'
-        # cargue el archivo en el directorio /tmp/
         put(archive_path, "/tmp/{}".format(archive))
-        # descromprimir el archvio /data/web_static/releases/{filename}
         run('mkdir -p {}{}/'.format(path, filename))
         run('tar -xzf /tmp/{} -C {}{}/'.format(archive, path, filename))
-        # Eliminar el archivo del servidor web
         run('rm /tmp/{}'.format(archive))
-        # mover a la ruta path
         run('mv {}{}/web_static/* {}{}/'.format(path, filename,
                                                 path, filename))
         run('rm -rf {}{}/web_static'.format(path, filename))
-        # Elimine el enlace simbólico /data/web_static/current del servidor web
         run('rm -rf /data/web_static/current')
-        # Crear un nuevo enlace simbólico
         run('ln -s {}{}/ /data/web_static/current'.format(path, filename))
         print('New version deployed!')
         return True
@@ -60,12 +52,9 @@ def do_deploy(archive_path):
 
 
 def deploy():
-    """ script (based on the file 2-do_deploy_web_static.py) that creates
-    and distributes an archive to your web servers, using the
-    function deploy"""
+    """deploys a file to your web servers, using the do_deploy function"""
     callFunction = do_pack()
 
     if callFunction is None:
         return False
-    new = do_deploy(callFunction)
-    return new
+    return do_deploy(callFunction)
