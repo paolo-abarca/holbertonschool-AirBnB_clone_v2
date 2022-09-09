f480eda3c856c996a9bc04e3ea9ea67fe1cb3500
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-09-09 14:39:46 -0500

    Adding a Python script that starts a Flask web app listening on 0.0.0.0, port 5000 and displays Python, followed by the value of the text variable. The text defaults to is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application:
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """
+    function that returns a Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """
+    function that returns a HBNB
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """
+    function that returns a c and text
+    """
+    result = text.replace("_", " ")
+    return "C {}".format(result)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is cool"):
+    """
+    function that returns a python and text
+    """
+    result = text.replace("_", " ")
+    return "Python {}".format(result)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
