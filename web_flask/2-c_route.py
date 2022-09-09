dc7faf87aee39fb517f1826453b3635d64b59832
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-09-09 11:37:50 -0500

    Add python script that starts a Flask web app listening on 0.0.0.0, port 5000, and /c/<text>: displays C followed by the value of the text variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application:
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """
+    function that returns a Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """
+    function that returns a HBNB
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """
+    function that returns a c and text
+    """
+    result = text.replace("_", " ")
+    return "C {}".format(result)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
