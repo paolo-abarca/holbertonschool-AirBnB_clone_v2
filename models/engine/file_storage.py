152d30bdf4cb0a04ce68a19b602ad9ef490bc550
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-09-10 18:58:14 -0500

    updating filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -62,3 +62,7 @@ class FileStorage:
 
             if key in self.__objects:
                 del self.__objects[key]
+
+    def close(self):
+        """method that call reload"""
+        self.reload()
