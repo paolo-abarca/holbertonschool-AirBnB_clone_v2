32df274a50aad337209be754ea241680626a3297
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-09-10 18:45:24 -0500

    updating db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """Adding a storage engine with SQLAlchemy"""
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
 from os import getenv
-from models.base_model import BaseModel, Base
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
+from models.base_model import Base
+from models.base_model import BaseModel
 from models.amenity import Amenity
+from models.city import City
+from models.place import Place
 from models.review import Review
+from models.state import State
+from models.user import User
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 
@@ -21,44 +22,36 @@ class DBStorage():
 
     def __init__(self):
         """Public instance methods that an engine creates"""
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(getenv('HBNB_MYSQL_USER'),
-                                              getenv('HBNB_MYSQL_PWD'),
-                                              getenv('HBNB_MYSQL_HOST'),
-                                              getenv('HBNB_MYSQL_DB')),
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
+                                      format(getenv("HBNB_MYSQL_USER"),
+                                             getenv("HBNB_MYSQL_PWD"),
+                                             getenv("HBNB_MYSQL_HOST"),
+                                             getenv("HBNB_MYSQL_DB")),
                                       pool_pre_ping=True)
-
-    if getenv('HBNB_ENV') == 'test':
-        Base.metadata.drop_all(self.__engine)
+        if getenv("HBNB_ENV") == "test":
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """ Methods for list the table"""
-        new_dict = {}
-        classes = [State, City]
-
-        if cls in classes:
-            obj = self.__session.query(cls).all()
-
-            for row in obj:
-                key = type(row).__name__ + "." + row.id
-                new_dict[key] = row
-
+        """Methods for list the table"""
+        if cls is None:
+            objs = self.__session.query(State).all()
+            objs.extend(self.__session.query(City).all())
+            objs.extend(self.__session.query(User).all())
+            objs.extend(self.__session.query(Place).all())
+            objs.extend(self.__session.query(Review).all())
+            objs.extend(self.__session.query(Amenity).all())
         else:
-            for clase in classes:
-                obj = self.__session.query(clase).all()
-
-                for row in obj:
-                    key = type(row).__name__ + "." + row.id
-                    new_dict[key] = row
-
-        return new_dict
+            if type(cls) == str:
+                cls = eval(cls)
+            objs = self.__session.query(cls)
+        return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
 
     def new(self, obj):
         """adds the object to the current database session"""
         self.__session.add(obj)
 
     def save(self):
-        """commits all changes to the current database session"""
+        """Commit all changes to the current database session"""
         self.__session.commit()
 
     def delete(self, obj=None):
@@ -69,12 +62,9 @@ class DBStorage():
     def reload(self):
         """create all tables in the database (SQLAlchemy function)"""
         Base.metadata.create_all(self.__engine)
-
-        """""create the current database session"""
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
-
         self.__session = Session()
 
     def close(self):
