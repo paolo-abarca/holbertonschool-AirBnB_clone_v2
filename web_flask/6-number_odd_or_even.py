75ca5132bea9cdb2f3a882f7dc161e5ee321dec5
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-09-09 22:29:58 -0500

    Added Python script that starts a Flask web app listening on 0.0.0.0, port 5000 and displays an HTML page only if n is an integer. H1 tag: Number: n is odd or even inside the body tag

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application:
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """
+    function that returns a Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """
+    function that returns a HBNB
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """
+    function that returns a c and text
+    """
+    result = text.replace("_", " ")
+    return "C {}".format(result)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is cool"):
+    """
+    function that returns a python and text
+    """
+    result = text.replace("_", " ")
+    return "Python {}".format(result)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_text(n):
+    """
+    function that returns a n and text
+    """
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """
+    function that returns a number template
+    """
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    """
+    function that returns a number odd or even
+    """
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
