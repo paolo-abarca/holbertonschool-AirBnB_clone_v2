87ea28e021460c68707533c3577da2b6ce62d482
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-09-11 22:05:00 -0500

    Adding user file

--- models/user.py
+++ models/user.py
@@ -9,10 +9,11 @@ class User(BaseModel, Base):
     """
     This class defines a user by various attributes
     """
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
     places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
